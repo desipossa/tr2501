2826d460fa344bd57d60503e4f68905de2a084ef
desipossa <c1jang@hanmail.net>
2025-01-21 12:59:59 +0900

    20250121

--- js/custom_nav01_r.js
+++ js/custom_nav01_r.js
@@ -12,7 +12,35 @@ $(function () {
             nextEl: '.main_visual .arrows .next',
             prevEl: '.main_visual .arrows .prev',
         },
-    })
+    });
+
+});
+
+
+// 모바일 메뉴
+
+$(function () {
+
+    $('.header .mbtn').on('click', function () {
+        $('.header .gnb').toggleClass('on')
+    });
+
+
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideToggle();
+            $(this).parent().siblings().find('ul').stop().slideUp();
+        }
+
+    });
+
+    $(window).on('resize', function () {
+        $('.header .gnb').removeClass('on')
+        $('.header .gnb>ul>li ul').removeAttr('style')
+    });
+
+
 });
 
 
