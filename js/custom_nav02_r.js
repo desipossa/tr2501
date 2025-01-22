85601bdb3a52d271683e1750cd86e7876228ce58
desipossa <c1jang@hanmail.net>
2025-01-22 11:55:02 +0900

    20250122

--- js/custom_nav02_r.js
+++ js/custom_nav02_r.js
@@ -12,38 +12,35 @@ $(function () {
             nextEl: '.main_visual .arrows .next',
             prevEl: '.main_visual .arrows .prev',
         },
-    });
-
+    })
 });
 
 
-// 모바일 메뉴
-
 $(function () {
-
     $('.header .mbtn').on('click', function () {
-        $('.header .gnb').toggleClass('on')
+        $(this).toggleClass('on');
+        $('.header .gnb').toggleClass('on');
     });
-
 
     $('.header .gnb>ul>li>a').on('click', function (e) {
         if ($('.header .gnb').hasClass('on')) {
             e.preventDefault();
-            $(this).next().stop().slideToggle();
-            $(this).parent().siblings().find('ul').stop().slideUp();
+            $('.header .gnb>ul ul').stop().slideUp();
+            $(this).next().stop().slideDown();
         }
 
     });
 
-
-
     $(window).on('resize', function () {
+        $('.header .mbtn').removeClass('on');
         $('.header .gnb').removeClass('on');
-        $('.header .gnb>ul>li ul').removeAttr('style')
+        $('.header .gnb>ul ul').removeAttr('style')
     });
 
-
-});
+    $('.header .gnb').on('wheel touchmove', function (e) {
+        e.preventDefault();
+    })
+})
 
 
 
